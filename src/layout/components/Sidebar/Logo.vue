3e6f27b0c9af3e7777b579f5314f0cd3e17d5d65
Emily Centonze <emily.centonze@gmail.com>
2021-05-12 14:21:48 -0400

    adding icons

--- src/layout/components/Sidebar/Logo.vue
+++ src/layout/components/Sidebar/Logo.vue
@@ -24,8 +24,8 @@ export default {
   },
   data() {
     return {
-      title: 'Vue Admin Template',
-      logo: 'https://wpimg.wallstcn.com/69a1c46c-eb1c-4b46-8bd4-e9e686ef5251.png'
+      title: 'Firstlight CUCM Configurator',
+      logo: 'https://www.firstlight.net/wp-content/themes/gt-tailwind/img/logo-white.svg'
     }
   }
 }
